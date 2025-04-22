3893830115ff2ad3681637009e15e740df674395
Michael <michael.onyango2@student.moringaschool.com>
2025-04-22 10:07:09 +0300

    1

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { image } from "../data/data";
+import { name, image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>My name is {name} and I am a web developer.</p>
+    <img src={image} alt="I made this" />
+    About
+  </div>
+  );
 }
 
 export default About;
